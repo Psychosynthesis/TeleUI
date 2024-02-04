d83655fb7bbb084bab1f564dfba164511c893c3f
Nick <ex-er-sis@ya.ru>
2024-02-04 22:34:14 +0300

    Update main

--- src/Button/types.ts
+++
@@ -1,9 +0,0 @@
-export type ButtonProps = {
-	children?: string | React.ReactNode;
-	onClick: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
-	type?: "outline";
-	size?: number;
-	color?: string;
-	className?: string;
-	disabled?: boolean;
-}
--- src/Loader/Loader.tsx
+++
@@ -1,9 +0,0 @@
-import './style.scss';
-
-export const Loader = () => {
-	return (
-		<div className="spinner-container">
-			<div className="spinner"/>
-		</div>
-	)
-}
