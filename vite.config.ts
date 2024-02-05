c18fc688d785a2fa2566793960b9b70f98647efc
Nick <ex-er-sis@ya.ru>
2024-02-05 06:50:22 +0300

    Trying to make it work

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,25 @@
 import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import ViteCSSExportPlugin from 'vite-plugin-css-export';
 import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const isProd = mode === 'production';
   return {
-    base: '',
     esbuild: {
       pure: isProd ? ['console.log'] : [],
     },
     // logLevel: mode === 'production' ? 'silent' : 'warn',
     // clearScreen: mode === 'production',
-    plugins: [ react() ],
+    plugins: [ react(), ViteCSSExportPlugin() ],
     build: {
       assetsDir: '', // Place all assets in root
       assetsInlineLimit: 1024, // Smaller resources will be embedded as base64
       sourcemap: false,
+      outDir: './build',
+      emptyOutDir: false, // avoid vite build deleting content of outDir
       lib: {
         entry: resolve(__dirname, './src/index.ts'),
         name: 'Telegram apstyle',
